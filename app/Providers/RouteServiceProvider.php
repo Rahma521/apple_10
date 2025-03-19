a3323d8f87f1b46a996be9413b79f0a534b07701
Rahma AbdEl Aziz <rahma.rere.44@gmail.com>
2025-03-19 12:58:45 +0200

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -33,7 +33,7 @@ class RouteServiceProvider extends ServiceProvider
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
 
-            Route::middleware('api')
+            Route::middleware('admin')
                 ->prefix('admin')
                 ->group(base_path('routes/admin.php'));
 
