109341a7ddfc8d1eb7963078e4861b04864f9203
Rahma AbdEl Aziz <rahma.rere.44@gmail.com>
2025-03-19 13:14:38 +0200

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -31,11 +31,11 @@ class RouteServiceProvider extends ServiceProvider
         $this->routes(function () {
             Route::middleware('api')
                 ->prefix('api')
-                ->group(base_path('routes/api.php'));
+                ->group(function () {
+                    require base_path('routes/api.php');
+                    require base_path('routes/admin.php');
+                });
 
-            Route::middleware('admin')
-                ->prefix('api')
-                ->group(base_path('routes/admin.php'));
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
