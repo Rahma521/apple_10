c9b693584aabd316d183c224b126cf94d6812e3c
Rahma AbdEl Aziz <rahma.rere.44@gmail.com>
2025-03-20 11:17:06 +0200

    user

--- app/Enums/UserTypeEnum.php
+++ app/Enums/UserTypeEnum.php
@@ -2,18 +2,18 @@
 
 namespace App\Enums;
 
-enum UserTypeEnum: string
+enum UserTypeEnum: int
 {
-    case instructor = '1';
-    case student = '2';
-    case other = '3';
+    case instructor = 1;
+    case student = 2;
+    case other = 3;
 
     public function label(): string
     {
         return match ($this) {
-            self::instructor => 'application.educator',
-            self::student =>'application.student',
-            self::other => 'application.other',
+            self::instructor => __('application.educator'),
+            self::student => __('application.student'),
+            self::other => __('application.other'),
         };
     }
 }
